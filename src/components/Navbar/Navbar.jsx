9a7a148e54cdb1fee6454d566746368839781bef
Deepak Verma <0822deepakverma@gmail.com>
2022-01-13 17:10:12 +0530

    navbar ready

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-
+import { FaSearchengin, FaSortDown, FaBell } from "react-icons/fa";
 import "./navbar.scss";
 
 const Navbar = () => {
@@ -13,19 +13,30 @@ const Navbar = () => {
           <span>Homepage</span>
           <span>Series</span>
           <span>Movies</span>
-            <span>TV Shows</span>
-            <span>My List</span>
+          <span>TV Shows</span>
+          <span>My List</span>
         </div>
         <div className="right">
-        
-            
-            <span>KID</span>
-           
-            <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+          <FaSearchengin className="icon" />
+          <span>KID</span>
+          <FaBell className="icon" />
+          
+
+          <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+          <div className="profile">
+          
+            <FaSortDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    
   );
 };
 
