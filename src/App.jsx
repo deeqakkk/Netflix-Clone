7d8fd034655d84a0a32888e52d7772dd061bd473
Deepak <0822deepakverma@gmail.com>
2022-01-30 11:37:35 +0530

    working on login component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import "./app.scss"
 import Home from "./Pages/Homepage/Home";
 import Watch from "./Pages/Watch/Watch";
 import Register from "./Pages/Register/Register";
+import Login from "./Pages/Login/Login";
 
 const App = () => {
-    return <Register / > ;
+    return <Login / > ;
 };
 
 export default App;
