76b5047a3e191e4183493162d47ed99d1e459417
Deepak <0822deepakverma@gmail.com>
2022-02-12 18:53:41 +0530

    authentication done

--- api/index.js
+++ api/index.js
@@ -2,20 +2,25 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch(err => {
-        console.log(err);
-    });
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
 
 app.listen(8800, () => {
-    console.log("Server is running on port 8800");
+  console.log("Server is running on port 8800");
 });
